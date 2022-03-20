bfdb33fc50de569cab2d627743be9273ad2e9bf7
laffey98 <1067116776@qq.com>
2022-03-20 19:35:06 +0800

    博客config读取（后端基本完成）

--- controllers/default.go
+++ controllers/default.go
@@ -2,13 +2,19 @@ package controllers
 
 import (
 	"hello/print"
+	"io/ioutil"
 
 	beego "github.com/beego/beego/v2/server/web"
+	"gopkg.in/yaml.v2"
 )
 
 const (
 	place = "controllers"
 )
+
+type Config struct {
+	Data map[int64]string
+}
 
 type MainController struct {
 	beego.Controller
@@ -31,7 +37,8 @@ type FileController struct {
 }
 
 type blog struct {
-	Id int64 `form:"id"`
+	Id   int64 `form:"id"`
+	name string
 }
 
 //------------------------------------------------
@@ -58,11 +65,30 @@ func (c *BlogController) Get() {
 		c.StopRun()
 	}
 	//test
+	/* 序号逻辑
+	二级页面：
+		#范围：1-99
+		博客归档：2
+		站内自用：3
+		关于我：4
+		QQ微信：5
+	三级页面：
+		#范围：10000000-99999999
+		具体博客：年月日+今日生成顺序	如：22031401
+	*/
+	config := Config{}
+	config.Config()
+	blog.name = config.Find(blog.Id)
 	print.Printvar(place, blog.Id)
+	print.Printvar(place, blog.name)
+	if blog.name == "nofind" {
+		c.Abort("Notfind")
+	}
+
 	switch {
 	case blog.Id > 0 && blog.Id < 100:
 		c.TplName = "NorAndMd.tpl"
-	case blog.Id > 100 && blog.Id < 1000:
+	case blog.Id > 10000000 && blog.Id < 99999999:
 		c.TplName = "NME.tpl"
 	default:
 		c.Abort("Notfind")
@@ -87,6 +113,7 @@ func (c *FileController) Post() {
 		}
 		defer f.Close()
 		c.SaveToFile("uploadname", "static/download/"+h.Filename)
+		c.Data["upload"] = true
 	} else {
 		c.Abort("Uploaderror")
 	}
@@ -108,4 +135,31 @@ func (c *ErrorController) ErrorNotfind() {
 func (c *ErrorController) ErrorUploaderror() {
 	//c.Data["content"] = "page not found"
 	c.TplName = "Uploaderror.tpl"
+}
+
+//------------------------------------------------
+//parse config
+//------------------------------------------------
+func (c *Config) Config() {
+	yamlS, readErr := ioutil.ReadFile("conf/blog.yaml")
+	if readErr != nil {
+		//fmt.Print(readErr)
+		print.Printerr(readErr, "ConfigParse")
+		return
+	}
+	// yaml解析的时候c.data如果没有被初始化，会自动为你做初始化
+	err := yaml.Unmarshal(yamlS, &c.Data)
+	if err != nil {
+		//fmt.Print(err)
+		print.Printerr(err, "ConfigPrase")
+	}
+	print.Printvar("ConfigParse", c.Data)
+}
+
+func (c *Config) Find(id int64) string {
+	if c.Data[id] == "" {
+		print.Printvar("ConfigParse", "Not Find")
+		return "nofind"
+	}
+	return c.Data[id]
 }
